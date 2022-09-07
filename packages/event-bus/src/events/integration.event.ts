a6eb4e1e9858e3167e4ea4ee2d656db666663f39
ludinhquan <ludinhquan@gmail.com>
2022-09-07 13:27:16 +0700

    update

--- packages/event-bus/src/events/integration.event.ts
+++ packages/event-bus/src/events/integration.event.ts
@@ -1,12 +1,9 @@
-import {UUID} from "@ilotustech/common";
-
 export abstract class IntegrationEvent {
   public readonly id: string;
 
   public readonly dateOccurred: number;
 
   constructor(props?: {id?: string, dateOccurred?: number}) {
-    this.id = props?.id ?? UUID.generate().unpack();
     this.dateOccurred = props?.dateOccurred ?? Date.now();
   }
 
